71b8e6c726875acc12d0f0485a866bc0e0ba8ac6
EeshanGit <eeshan.prasannamarathe@students.iiserpune.ac.in>
2025-04-06 14:36:41 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eeshangit.github.io',
-  // base:,
+  base: '/src/pages',
   integrations: [mdx(), sitemap(), tailwind()]
 });
