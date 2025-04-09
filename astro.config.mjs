671312a33b82fc32a7f6cb05893231f7097ce649
EeshanGit <eeshan.prasannamarathe@students.iiserpune.ac.in>
2025-04-09 12:54:51 +0530

    Update astro.config.mjs - again again again again

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eeshangit.github.io',
-  //base:"/Kalpa_Build_Test",
   integrations: [mdx(), sitemap(), tailwind()]
 });
